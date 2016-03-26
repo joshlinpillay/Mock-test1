b920df8cb94953e377f8290d39389f7c25344b93
joshlinpillay <joshlinpillay12@gmail.com>
2016-03-26 22:16:32 +0200

    ques1

---
+++ Source.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+
+// ***** QUESTION 1
+// Question 1 consists of 5 parts (order and approach is critical)
+
+
+int main(int argc, char *argv[]) {
+
+	cout << "This is the first line of code" << endl;
+
+
+	
